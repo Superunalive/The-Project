bba0d1cec51fed1ed7ae26bc406de14f6d6f973f
Superunalive <161376015+Superunalive@users.noreply.github.com>
2024-05-25 23:38:59 +0300

    Update spells.h
    Constructors and SetName

--- libraries/spells.h
+++ libraries/spells.h
@@ -5,6 +5,17 @@ class Spell{
     int damage_; //how much damage. =0 deals no damage and <0 heals
     int behaviour_; //what else it does
     public:
+    Spell(){
+        name_ = "placeholder";
+        mana_, damage_ = 0;
+        behaviour_ = 1;
+    }
+    Spell(Spell &other){
+        (*this).SetName(other.name_);
+        (*this).SetMana(other.mana_);
+        (*this).SetDamage(other.damage_);
+        (*this).SetBehaviour(other.behaviour_);
+    }
     char* GetName(){
         return name_;
     }
@@ -17,7 +28,9 @@ class Spell{
     int GetBehaviour(){
         return behaviour_;
     }
-
+    void SetName(const char* name){
+        this->name_ = name;
+    }
     void SetDamage(int damage){
         this->damage_ = damage;
     }
