7f8123755558f4a9d82912443c3cf3c2a30bb945
Mikhail Shadrin <mishatheg@gmail.com>
2024-06-02 22:46:33 +0300

    factory + cleanup

--- libraries/heroes.h
+++ libraries/heroes.h
@@ -10,7 +10,6 @@ class Hero {
     int damage_; //name, this and health are self-explanatory. c means current, m means maximum
     int mana_suck_; //placeholder name, how much mana player receives for dealing damage with this hero
     int behaviour_; //corresponds to an ability function
-    int modifier_; //placeholder for ability strength-defining number
     
     public:
 
@@ -23,7 +22,6 @@ class Hero {
     int GetDamage();
     int GetManaSuck();
     int GetBehaviour();
-    int GetModifier();
 
     void Set(char *name, int health, int damage, int mana_suck, int behaviour, int modifier);
     void SetName(char *name);
@@ -32,14 +30,51 @@ class Hero {
     void SetDamage(int damage);
     void SetManaSuck(int mana_suck);
     void SetBehaviour(int behaviour);
-    void SetModifier(int modifier);
     
     ~Hero();
 
-
+    friend void behaviour(int& x, int& y, Cell** board, int nx, int ny, char l,int& kills_one = nullptr, int& kills_two = nullptr, std::vector<int>& x1 = nullptr,std::vector<int>& y1 = nullptr,std::vector<int>& x2 = nullptr, std::vector<int>& y2 = nullptr);
     Hero &operator=(Hero & other);
     friend std::ifstream &operator>>(std::ifstream&, Hero&);
 };
-
-
+class Basic : public Hero{
+    public:
+    using Hero::Hero:
+    behaviour (int& x, int& y, Cell** board, int nx, int ny, char l) {
+        int x1 = x;
+        int y1 = y;
+        step_pos(x1,y1,type,hp,nx,ny,l,dm);
+        change_pos(x,y,x1,y1,type,hp);
+        damage(x1,y1,type,hp,nx,ny,l,3);
+        counter_attack(x1,y1,type,hp,nx,ny,l);
+    }
+}
+class Strikethrough : public Hero{
+    public:
+    using Hero::Hero:
+    behaviour (int& x, int& y, Cell** board, int nx, int ny, char l) {
+        int x1 = x;
+        int y1 = y;
+        step_pos(x1,y1,type,hp,nx,ny,l,dm);
+        change_pos(x,y,x1,y1,type,hp);
+        //pierce_damage(x1,y1,type,hp,nx,ny,l);
+        counter_attack(x1,y1,type,hp,nx,ny,l);
+    }
+}
+class Dodging : public Hero{
+    public:
+    using Hero::Hero:
+    behaviour (int& x, int& y, Cell** board, int nx, int ny, char l,int& kills_one, int& kills_two,std::vector<int>& x1,std::vector<int>& y1,std::vector<int>& x2, std::vector<int>& y2) {
+        int x1 = x;
+        int y1 = y;
+        step_pos(x1,y1,type,hp,nx,ny,l,turn);
+        change_pos(x,y,x1,y1,type,hp);
+        damage(x1,y1,type,hp,nx,ny,l,3);
+        int x2 = x1;
+        int y2 = y1;
+        evasion_pos(x2,y2,type,hp,nx,ny,l,turn); 
+        change_pos(x1,y1,x2,y2,type,hp);
+        counter_attack(x1,y1,type,hp,nx,ny,l);
+    }
+}
 #endif
--- main_code/board.h
+++
@@ -1,6 +0,0 @@
-#include "../libraries/heroes.h"
-struct Cell{
-    Hero* hero; //nullptr = no hero. Walls are also heroes
-    bool IsBusy; //can someone stand on/go through this cell
-    int special_effect;
-};
